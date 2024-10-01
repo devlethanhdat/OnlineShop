b728279b62abe56870dd467de3eb3752ffaeef81
DatDev <datthichlaptrinh@gmail.com>
2024-10-02 02:28:07 +0700

    Banners & Sliders (BE)

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Models;
+using OnlineShop.Models.Db;
 using System.Diagnostics;
 
 namespace OnlineShop.Controllers
@@ -7,14 +8,18 @@ namespace OnlineShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly OnlineShopContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, OnlineShopContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var banners = _context.Banners.ToList();
+            ViewData[nameof(banners)] = banners;
             return View();
         }
 
